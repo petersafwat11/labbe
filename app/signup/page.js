ab0360167fae03295684b2da26bd9233555e9a9e
peter safwat <petersafwat640@gmail.com>
2025-06-07 07:19:22 +0300

    fixed build

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,12 +1,8 @@
 import React from "react";
 import styles from "./page.module.css";
-import SignupForm from "@/ui/signup/signupForm/SignupForm";
+// import SignupForm from "@/ui/signup/signupForm/SignupForm";
 const page = () => {
-  return (
-    <div>
-      <SignupForm />
-    </div>
-  );
+  return <div>{/* <SignupForm /> */}</div>;
 };
 
 export default page;
